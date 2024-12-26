1377e95fe352338816d0b11f302990b30ffcb45c
hsarpitha <arpithakotegar8@gmail.com>
2024-12-26 12:52:37 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
+import Contact from './Contact'; 
+import About from './About';
+import Greeting from './Greeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Hello from React JS</h1>
+    <Greeting name = {'Akash'}/>
+    <Greeting name = {'Sahana'}/>
+    {/* <Home name = {'Arna'} age = {21} />  */}
+    {/* <Contact /> */}
+    {/* <About /> */}
     </div>
   );
 }
